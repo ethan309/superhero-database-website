d53cfd4a261a45ac62557fff6750bb65dc5f03c6
Ethan Butt <ethanbutt309@gmail.com>
2020-11-15 15:50:35 -0600

    feat: Add component for page NavBar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="NavBar">
+    <h1>Hello App!</h1>
+    <p>
+        <!-- use router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+  data: function() {
+    return {
+        
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
