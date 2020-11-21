9d18874621da406d68620898f7aa2999240f054c
Ethan Butt <ethanbutt309@gmail.com>
2020-11-21 14:58:32 -0600

    feat: show any one stat for all available characters.

--- src/pages/Explore.vue
+++ src/pages/Explore.vue
@@ -6,7 +6,6 @@
     <div>View by intelligence</div>
     <div>View by strength</div>
     <div v-show="resultsFound" id="stats"></div>
-    <div v-show="resultsFound" id="powers"></div>
   </div>
 </template>
 
@@ -23,11 +22,11 @@ export default {
       };
   },
   async mounted() {
-    const characterData = await axios.get(`/api/characters/`);
-    console.log(characterData);
+    const characterData = await axios.get(`/api/characters`);
+    
     if(characterData.status === 200) {
       this.parseCharacterData(characterData.data);
-      this.displayStatsGraph();
+      this.displayStatsGraph('Intelligence');
       this.resultsFound = true;
     } else {
       this.resultsFound = false;
@@ -37,70 +36,64 @@ export default {
     parseCharacterData: function(data) {
       this.data = data;
     },
-    displayStatsGraph: function() {
-      // const numericalStats = this.stats.filter((stat) => { return typeof stat.value === 'number' });
+    displayStatsGraph: function(stat) {
+      const data = [];
+      this.data.forEach((currectCharacter) => {
+        if(currectCharacter[stat] !== '') {
+          data.push({ 'name': currectCharacter['Name'], 'value': currectCharacter[stat] });
+        }
+      });
 
-      // Template from https://bl.ocks.org/mbostock/3885304
-      var svg = d3.select("#stats"),
-          margin = {top: 20, right: 20, bottom: 30, left: 40},
-          width = 500 - margin.left - margin.right,
-          height = 500 - margin.top - margin.bottom;
-      
-      //Generates a categorical x axis and a numeric y axis
-      var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-          y = d3.scaleLinear().rangeRound([height, 0]);
-      
-      //Generates chart container that is adjusted by margin parameters
-      var g = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      console.log('1');
-      //Reads data from tsv
-      x.domain(this.data.map(function(d) { 
-          if (d.Intelligence !== ''){
-            return d.name
-          }
-        }));
-      y.domain([0, d3.max(this.data, function(d) { return d.Intelligence; })]);
+      // set the dimensions and margins of the graph
+      var margin = {top: 20, right: 30, bottom: 40, left: 90};
+      var width = 1600 - margin.left - margin.right;
+      var height = (10 * data.length) - margin.top - margin.bottom;
 
-      // d3.tsv("data.tsv", function(d) {
-      //   //set up your data transforms here
-      //   d.name = "";
-      //   d.value = + 0;
-      //   return d;
-      // }, function(error, data) {
-      //   if (error) throw error;
-      
-        //Sets categorical x domain based on given column
-      
-        //Draw x axis and adjust for container size
-      g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
-        //Draw y axis with various styling and transforms
-      g.append("g")
-          .attr("class", "axis axis--y")
-          .call(d3.axisLeft(y).ticks(10, "%"))
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("text-anchor", "end")
-          .text("Value");
-        //Draw bars for each name and value in data
-      const sorted = []; 
-      this.data.forEach(item => {if (item.Intelligence != "" && item.Intelligence != undefined) { sorted.push(item) }});
-      console.log(sorted);
-      g.selectAll("#stats")
-        .data(sorted)
-        .enter().append("rect")
-          .attr("id", "stats")
-          .attr("x", function(d) { return x(d.Name); })
-          .attr("y", function(d) { return y(d.Intelligence); })
-          .attr("width", x.bandwidth())
-          .attr("height", function(d) { return height - y(d.Intelligence); });//draws bars on screen from top to bottom
-      console.log('2');
+      // append the svg object to the body of the page
+      var svg = d3.select("#stats")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
+      // Add X axis
+      var x = d3.scaleLinear()
+        .domain([0, 125])
+        .range([ 0, width]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+          .attr("transform", "translate(-10,0)rotate(-45)")
+          .style("text-anchor", "end");
+
+      // Y axis
+      var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(data.map(function(d) { return d.name; }))
+        .padding(.1);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+
+      //Bars
+      svg.selectAll("myRect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", x(0) )
+        .attr("y", function(d) { return y(d.name); })
+        .attr("width", function(d) { return x(d.value); })
+        .attr("height", 5 ) // y.bandwidth()
+        .attr("fill", "#69b3a2")
+
+
+        // .attr("x", function(d) { return x(d.name); })
+        // .attr("y", function(d) { return y(d.value); })
+        // .attr("width", x.bandwidth())
+        // .attr("height", function(d) { return height - y(d.value); })
+        // .attr("fill", "#69b3a2")
     }
   }
 }
