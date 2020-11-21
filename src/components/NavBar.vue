f5539b8497113d0eeb4915c45e3228f2c240a3c6
Ethan Butt <ethanbutt309@gmail.com>
2020-11-20 23:53:13 -0600

    feat: actually show icons in NavBar links.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,9 +3,14 @@
       <router-link id='home' to="/Home">
         <img alt="Main logo" src="../assets/main.png">
       </router-link>
-      <ul v-for="link in links" v-bind:key="link.title">
-        <router-link class="link" v-bind:to="'/' + link.destination">
-          <img v-bind:alt="link.title" v-bind:src="'../assets/' + link.asset">
+      <ul>
+        <router-link class="link" to="/Search">
+          <img class="link-img" alt="Search" src="../assets/query.svg"><br>
+          <label>Search</label>
+        </router-link>
+        <router-link class="link" to="/Explore">
+          <img class="link-img" alt="Explore" src="../assets/explore.svg"><br>
+          <label>Explore</label>
         </router-link>
       </ul>
     </div>
@@ -44,9 +49,7 @@ export default {
   color: #42b983;
   float: right;
   display: block;
-  /* padding: 10px;
-  padding-top: 50px;
-  padding-bottom: 50px; */
+  padding: 10px;
   align-self: center;
 }
 ul {
@@ -58,6 +61,12 @@ ul {
 img {
   height: 100px;
   object-fit: contain;
+  align-self: center;
+}
+.link-img {
+  height: 70px;
+  object-fit: contain;
+  align-self: center;
 }
 
 </style>
