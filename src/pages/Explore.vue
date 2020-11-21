754527fbb4a34f295d0acad22ffff90670350e71
Ethan Butt <ethanbutt309@gmail.com>
2020-11-21 16:16:19 -0600

    wip: select stat in Explore.

--- src/pages/Explore.vue
+++ src/pages/Explore.vue
@@ -6,12 +6,14 @@
     <div>View by intelligence</div>
     <div>View by strength</div>
     <div v-show="resultsFound" id="stats"></div>
+    <div v-show="resultsFound" id="details"></div>
   </div>
 </template>
 
 <script>
 import * as d3 from 'd3';
 import axios from 'axios';
+import { statsColumns } from '../constants.js';
 
 export default {
   name: 'Explore',
@@ -20,6 +22,11 @@ export default {
         data: [],
         resultsFound: false
       };
+  },
+  computed: {
+    statOptions: function() {
+      return statsColumns;
+    }
   },
   async mounted() {
     const characterData = await axios.get(`/api/characters`);
@@ -38,16 +45,25 @@ export default {
     },
     displayStatsGraph: function(stat) {
       const data = [];
-      this.data.forEach((currectCharacter) => {
-        if(currectCharacter[stat] !== '') {
-          data.push({ 'name': currectCharacter['Name'], 'value': currectCharacter[stat] });
+      this.data.forEach((currentCharacter) => {
+        if(currentCharacter[stat] !== '') {
+          const existing = data.findIndex((entry) => entry['value'] === currentCharacter[stat])
+          if(existing === -1) {
+            data.push({ 'names': [currentCharacter['Name']], 'value': currentCharacter[stat] });
+          } else {
+            data[existing]['names'].push(currentCharacter['Name']);
+          }
         }
       });
 
+      data.sort(function(a, b) {
+        return d3.descending(a.value, b.value)
+      });
+
       // set the dimensions and margins of the graph
-      var margin = {top: 20, right: 30, bottom: 40, left: 90};
+      var margin = {top: 20, right: 80, bottom: 40, left: 90};
       var width = 1600 - margin.left - margin.right;
-      var height = (10 * data.length) - margin.top - margin.bottom;
+      var height = (20 * data.length) - margin.top - margin.bottom;
 
       // append the svg object to the body of the page
       var svg = d3.select("#stats")
@@ -57,11 +73,19 @@ export default {
         .append("g")
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
+      var svg_details = d3.select("#details")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
       // Add X axis
+      const MAX_X = 125;
       var x = d3.scaleLinear()
-        .domain([0, 125])
-        .range([ 0, width]);
+        .domain([ 0, MAX_X ])
+        .range([ 0, width ]);
       svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
@@ -72,10 +96,15 @@ export default {
       // Y axis
       var y = d3.scaleBand()
         .range([ 0, height ])
-        .domain(data.map(function(d) { return d.name; }))
+        .domain(data.map(function(d) { return `${d.value} (${d.names.length} names)`; }))
         .padding(.1);
       svg.append("g")
         .call(d3.axisLeft(y));
+
+      var tooltip = svg_details.append("text")
+        .style("visibility", "hidden")
+        .style("width", `${width}px`)
+        .style("overflow-wrap", "break-word");
 
       //Bars
       svg.selectAll("myRect")
@@ -83,17 +112,26 @@ export default {
         .enter()
         .append("rect")
         .attr("x", x(0) )
-        .attr("y", function(d) { return y(d.name); })
+        .attr("y", function(d) { return y(`${d.value} (${d.names.length} names)`); })
         .attr("width", function(d) { return x(d.value); })
-        .attr("height", 5 ) // y.bandwidth()
+        .attr("height", 10 ) // y.bandwidth()
         .attr("fill", "#69b3a2")
-
-
-        // .attr("x", function(d) { return x(d.name); })
-        // .attr("y", function(d) { return y(d.value); })
-        // .attr("width", x.bandwidth())
-        // .attr("height", function(d) { return height - y(d.value); })
-        // .attr("fill", "#69b3a2")
+        .on("mouseover", function(d, barData) {
+            // var tipx = 10 * i;
+            // var tipy = d3.select(this).attr("height");
+            d3.select(this).attr("r", 10).style("fill", "orange");
+            tooltip.attr("x", 2);
+            tooltip.attr("y", 2);
+            // tooltip.attr("dx", ((width/MAX_X) * barData.value) + 2);
+            // tooltip.attr("dy", y(barData.value) + 5);
+            tooltip.style("visibility", "visible");
+            tooltip.style("fill", "black");
+            tooltip.text(`${barData.value}: ${barData.names.join(', ')}`);
+        })
+        .on("mouseout", function() {
+            d3.select(this).attr("r", 10).style("fill", "#69b3a2");
+            tooltip.style("visibility", "hidden");
+        });
     }
   }
 }
