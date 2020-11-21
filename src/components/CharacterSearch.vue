4b422f072cd65b5d728a1510a7331549832399b0
Ethan Butt <ethanbutt309@gmail.com>
2020-11-20 22:22:41 -0600

    feat: connect search page with database.

--- src/components/CharacterSearch.vue
+++ src/components/CharacterSearch.vue
@@ -7,17 +7,22 @@
     <br>
     <div id="results" v-show="characterName.length > 0">
       <p>Results for {{characterName}}:</p>
-      <ul v-for="match in searchResults" v-bind:key="match.name">
+      <p v-show="searchResults.length === 0">No matches. Double-check your search term.</p>
+      <p v-show="error" class="error">There was an error completing your search. Please try again.</p>
+      <div id="matches">
+        <ul v-for="match in searchResults" v-bind:key="match.name">
         <router-link v-bind:to="{ name: 'Details', params: { id: match._id }}">
           <CharacterCard v-bind:name="match.name"/>
         </router-link>
       </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import CharacterCard from './CharacterCard.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -28,12 +33,22 @@ export default {
     return {
         query: '',
         characterName: '',
-        searchResults: [{ _id: '1234', name: 'Match 01' }, { _id: '5678', name: 'Match 02' }]
+        searchResults: [],
+        queryError: false
       };
   },
   methods: {
-    updateSearch: function(){
+    updateSearch: async function(){
       this.characterName = this.query;
+      const characters = await axios.get(`/api/characters/${this.characterName}`);
+      if(characters.status === 200) {
+        this.searchResults = characters.data.reduce((allFilteredCharacters, currectCharacter) => 
+          [ ...allFilteredCharacters, {'_id': currectCharacter['_id'], 'name': currectCharacter['Name']} ],
+          []
+        );
+      } else {
+        this.queryError = true;
+      }
     }
   }
 }
@@ -47,5 +62,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#matches {
+  display: flex;
+  flex-flow: row wrap;
+  max-height: 700px;
+  overflow: hidden;
 }
 </style>
